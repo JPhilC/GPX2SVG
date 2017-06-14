e7ded937dc3f79935bcdf5d47ea9cede41dfe46b
Phil Crompton <phil@unitysoftware.co.uk>
2017-06-14 15:25:59 +0100

    Remove unnecessary code from Main function.

--- GPX2SVG/Program.cs
+++ GPX2SVG/Program.cs
@@ -16,20 +16,25 @@ namespace GPX2SVG
          GPXLoader loader = new GPXLoader();
          Console.WriteLine("Processing file: {0}", fileName);
          IEnumerable<GPXRoute> routes = loader.LoadGPXRoutes(fileName);
-         StringBuilder sb = new StringBuilder();
-         foreach (GPXRoute route in routes) {
-            // Populate track data objects. 
-            foreach (Coordinate wp in route.WayPoints) {
-               // Populate detailed track segments 
-               // in the object model here. 
-               sb.Append(
-                 string.Format("Route:{0} - Latitude:{1} Longitude:{2} " +
-                              "Elevation:{3} or ({4},{5})\n",
-                 route.Name, wp.Latitude,
-                 wp.Longitude, wp.Elevation,
-                 wp.X, wp.Y));
-            }
-         }
+
+         //StringBuilder sb = new StringBuilder();
+         //foreach (GPXRoute route in routes) {
+         //   // Populate track data objects. 
+         //   foreach (Coordinate wp in route.WayPoints) {
+         //      // Populate detailed track segments 
+         //      // in the object model here. 
+         //      sb.Append(
+         //        string.Format("Route:{0} - Latitude:{1} Longitude:{2} " +
+         //                     "Elevation:{3} or ({4},{5})\n",
+         //        route.Name, wp.Latitude,
+         //        wp.Longitude, wp.Elevation,
+         //        wp.X, wp.Y));
+         //   }
+         //}
+         //System.Console.WriteLine(sb.ToString());
+         //System.Console.WriteLine("Press any key to exit");
+         //System.Console.ReadKey();
+
          SVGWriter writer = new SVGWriter();
          System.Console.WriteLine("Writing file: {0}", outputFile);
          writer.WriteRoute(routes.First(), 0.08, outputFile);
